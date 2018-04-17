4b417c5312630bd1dcbe822817cf35048136fdb0
Jimmy Moon <ragingwind@gmail.com>
2018-04-18 00:16:23 +0900

    add sdk-only

---
+++ sdk-only/helloworld.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <emscripten/emscripten.h>
+
+const char* HELLO_WORLD = "Hello World!";
+
+EMSCRIPTEN_KEEPALIVE
+const char* getString() {
+  return HELLO_WORLD;
+}
+
+unsigned int getLength() {
+  unsigned int len = 0;
+
+  for (const char* p = HELLO_WORLD; *p != '\0'; p++) {
+    len++;
+  }
+
+  return len;
+}
